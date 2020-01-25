b3222fbf3c9ce4be8904628753ff0758b83624ba
fernando <fernandofitilan@hotmail.com>
2020-01-24 21:41:04 -0300

    2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -35,6 +35,7 @@ $app = new Laravel\Lumen\Application(
 | your own bindings here if you like or you can make another file.
 |
 */
+$app->configure('swagger-lume');
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
